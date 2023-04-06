123a8cd93e61714dac3d051c45e72327ab0ebcfa
it21001802 <89139730+it21001802@users.noreply.github.com>
2023-04-06 19:43:33 +0530

    first admin backend commit

--- server/server.js
+++ server/server.js
@@ -1,39 +1,41 @@
 require('dotenv').config() 
 
 const express = require('express')
+
 const mongoose = require('mongoose')
 
-//routes
-const workoutRoutes = require('./routes/workouts')
-const volunteerRoutes = require('./routes/volunteerRoutes')
+// //routes
+// const workoutRoutes = require('./routes/workouts')
+// const volunteerRoutes = require('./routes/volunteerRoutes')
+const adminRoutes = require('./routes/adminRoutes')
 
 //express app
 const app = express();
 
-//middleware
+// //middleware
 app.use(express.json())
+
 app.use((req,res,next)=>{
     console.log(req.path,req.method)
     next()
 })
 
 
-//routes
-app.use('/api/workouts',workoutRoutes)
-app.use('/volunteer/delivery-jobs',volunteerRoutes)
-
+// //routes
+// app.use('/api/workouts',workoutRoutes)
+// app.use('/volunteer/delivery-jobs',volunteerRoutes)
+app.use('/admin/approves',adminRoutes)
 
 //Connect to db
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI1)
     .then(()=>{
         //listen for requests 
-        app.listen(process.env.PORT, () => {
-            console.log('connected to DB & listening on port', process.env.PORT )
+        app.listen(process.env.PORT1, () => {
+            console.log('connected to DB & listening on port', process.env.PORT1 )
         })
     })
     .catch((error) => {
         console.log(error)
     })
 
-
-process.env
+// process.env
